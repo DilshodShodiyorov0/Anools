ba820de96481ad2198c467905b9b9cf27142650e
DilshodShodiyorov0 <shodiyorovdilshod23@gmail.com>
2024-06-14 18:50:46 +0500

    111

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let array = [
+    "red",
+    'black',
+    'blue',
+    'green',
+] 
+let array_2 = array.slice(1, 2);
+
+// let array_1 = [
+//     "violer",
+//     "darkcrayn"
+// ]
+// let all_array = array.concat(array_1)
+// array.unshift("Light"); // Push - boshidan qiymat qo'shadi
+// array.push("orange"); // Push - oxiridan qiymat qo'shadi
+
+// array.shift(); // Shift - boshidan qiymatni olib tashlaydi
+// array.pop() // Pop - oxiridan qiymatni olib tashlaydi
+
+console.log(array_2);
+
+
+
